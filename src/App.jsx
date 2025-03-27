ec8e4b2184e34668be8db217670b1977c2a1a53e
TarunTechie <taruntechie25@gmail.com>
2025-03-28 02:12:44 +0530

    added custom cursor to home page

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className='customCursor bg-[url(/images/home.jpg)] w-screen h-screen bg-re'>
       <Header/>
       <div className='flex h-[84vh] content-center justify-center'>
-      <button onClick={openScoreBoard} className='bg-white px-10 m-auto w-fit py-4 rounded-2xl border-4 border-dashed'>Start Race 🏁</button>
+      <button onClick={openScoreBoard} className='bg-white px-10 m-auto w-fit py-4 rounded-2xl border-4 border-dashed customCursor'>Start Race 🏁</button>
       </div>
     </div>
   )
